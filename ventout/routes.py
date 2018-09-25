b78e15ced70ab44a36c2ae4c4520eedae28b2542
Nikhil <nikhil.prabhakar@mountblue.io>
2018-09-25 13:17:59 +0530

    Done

--- ventout/routes.py
+++ ventout/routes.py
@@ -1,5 +1,5 @@
 from ventout.models import User, Post
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from ventout.forms import RegistrationForm, LoginForm, PostForm
 from flask import Request
 from flask_login import login_user, current_user, logout_user, login_required
@@ -62,9 +62,11 @@ def logout():
 @app.route("/account")
 @login_required
 def account():
+    my_posts = Post.query.filter_by(user_id=current_user.id).all()
     image_file = url_for(
         'static', filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html', title='account', image_file=image_file)
+
+    return render_template('account.html', title='account', image_file=image_file, posts=my_posts)
 
 
 @app.route("/post/new", methods=['GET', 'POST'])
@@ -78,9 +80,41 @@ def new_post():
         db.session.commit()
         flash('Your Post is Vented', 'success')
         return redirect(url_for('home'))
-    return render_template('create_post.html', title='New Post', form=form)
+    return render_template('create_post.html', title='New Post', form=form, legend="Vent it Out")
+
 
 @app.route("/post/<int:post_id>")
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash("Post is successfully updated", 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post', form=form, legend="Update Post")
+
+
+@app.route("/post/<int:post_id>/delete", methods=['GET', 'POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash("Post is successfully deleted", 'success')
+    return redirect(url_for('home'))
